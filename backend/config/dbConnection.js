a5265388343baa28b05529c2061d3a2f4d222ece
Salini <iamsalinivijayan@gmail.com>
2023-12-01 08:46:25 +0530

    Updated the dbConnection.js file

---
+++ backend/config/dbConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const connectDb = async () =>{
+    try {
+        const connection = await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log("database connected", connection.connection.host, connection.connection.name )
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDb
