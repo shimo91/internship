0c79b7245251b5b470cb43efe7eff92e1c8bf155
shimo91 <shimonaprakash@gmail.com>
2023-12-02 16:50:48 +0400

    changes in forum

--- backend/Routes/Sdashboard.js
+++ backend/Routes/Sdashboard.js
@@ -2,7 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose');
 const router = express.Router()
 const cors = require('cors');
-const student=require('../Models/Studentdata.js');
+const student=require('../Models/StudentTopic');
 const app=new express();
 const jwt = require('jsonwebtoken');
 
