adc9503bc5aaec4d6f7f87ffc016f82a4bd92050
Ayana1402 <ayanagangadharan1402@gmail.com>
2023-12-09 20:34:09 +0530

    final report changes

--- frontend/src/elements/FinalReport.jsx
+++ frontend/src/elements/FinalReport.jsx
@@ -3,23 +3,46 @@ import { Container, Typography, TextField, TextareaAutosize, Button, Snackbar } 
 import axios from 'axios';
 import { Box} from '@mui/material';
 import { Grid} from '@mui/material'
+import { jwtDecode } from "jwt-decode";
 
 
 const FinalReport = () => {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState(null);
   const [downloadfile, setDownloadfile] = useState([]);
   const [hasUploaded, setHasUploaded] = useState(false);
-  const [username, setUsername] = useState(''); // Add this line
+  const [username, setUsername] = useState('');
+  const [warning, setWarning] = useState('');
+  const [completionDate, setCompletionDate] = useState(null);
+  const [futureDate, setFutureDate] = useState(null);
+
+  const token = sessionStorage.getItem("userToken");
+  const decodeToken = jwtDecode(token);
+  const userId = decodeToken.userid;
+
+  const currentDate = new Date();
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:4000/login/getuser/' + userId)
+      .then(res => {
+        const start_date = res.data.start_date;
+        const formatedcompleteddate = new Date(start_date);
+        setCompletionDate(formatedcompleteddate);
+
+        const futureDateValue = new Date(formatedcompleteddate);
+        futureDateValue.setDate(formatedcompleteddate.getDate() + 20);
+        setFutureDate(futureDateValue);
+
+        console.log("completiondate", formatedcompleteddate);
+        console.log('futuredate', futureDateValue);
+      })
+      .catch(error => {
+        console.error("Error fetching data from the server:", error);
+      });
+  }, []);
 
   useEffect(() => {
     const storedUsername = sessionStorage.getItem('username');
     setUsername(storedUsername);
-    handledownload();
-  }, [])
-
-
- 
-  useEffect(() => {
     handledownload();
   }, []);
 
@@ -43,33 +66,35 @@ const FinalReport = () => {
 
   function handleFile(event) {
     setFile(event.target.files[0]);
-    
-    setHasUploaded(false); // Reset the flag when a new file is selected
+    setHasUploaded(false);
   }
 
   const handleUpload = async (e) => {
     e.preventDefault();
-    //to check username 
-    const isUsernameExists = downloadfile.some((data) => data.username === username);
 
-    if (isUsernameExists) {
-      alert('Cannot upload.Report already submitted');
+    if (!futureDate || !completionDate) {
+      console.error('futureDate or completionDate is not defined. Make sure the data is loaded.');
       return;
     }
-    
+
+    const isUsernameExists = downloadfile.some((data) => data.username === username);
+    if (futureDate.getTime() > currentDate.getTime()) {
+      setWarning(<span style={{ color: 'red' }}>Upload report allowed on or after {futureDate.toLocaleDateString()}.</span>);
+      return;
+    } else if (futureDate.getTime() <= currentDate.getTime() && isUsernameExists) {
+      alert('Cannot upload. Report already submitted');
+      return;
+    }
 
     const formData = new FormData();
     formData.append('file', file);
     formData.append('username', username);
-    console.log(username)
-    console.log(file);
-   
 
     try {
       const result = await axios.post('http://localhost:4000/report/upload', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
-      
+
       setHasUploaded(true);
       alert('Final Report successfully submitted');
       window.location.reload(false);
@@ -111,7 +136,11 @@ const FinalReport = () => {
    <div className="repo-img"></div>
    <br></br><br></br><br></br>
    <form onSubmit={handleUpload}>
-        {/* <h1>Submission Page</h1> */}
+   {warning}
+      {completionDate && (
+        <p>Last date to submit Report: {completionDate.toLocaleDateString()}</p>
+
+      )}
         <input type="file" className="formcontrol" name="file" accept="application/pdf" onChange={handleFile} required />
         <input type="text" className="formcontrol" name="username" value={username} onChange={handleFile} required />
         <button type="submit" disabled={hasUploaded} >
@@ -120,11 +149,7 @@ const FinalReport = () => {
         <br></br><br></br><br></br>
       </form>
       <div>
-        {/* {downloadfile.map((data) => (
-          <button key={data.filename} type="submit" onClick={() => showfile(data.filename,data.username)}>
-            View Report
-          </button>
-        ))} */}
+       
         {userReports.length > 0 && (
           <button type="submit" onClick={showfile}>
             View Report
